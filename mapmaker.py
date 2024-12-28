83b4bc8cdb4782029cc739c98b2183a6b180f3c6
aapo <aapo740@gmail.com>
2024-12-28 15:47:41 +0200

    korjattu mapmaker ja muuta

--- mapmaker.py
+++ mapmaker.py
@@ -12,16 +12,16 @@ def round_to_multiple(number, multiple):
 
 def ParseList():
     newList = []
-    for i in range(0, len(allBlocks)):
+    for i in range(0, len(allBlocks), 1):
         bX = allBlocks[i][0]
         bY = allBlocks[i][1]
-        if f"{bX},{bY}:{bX + 1},{bY}" not in allBlocks:
+        if f"{bX},{bY}:{bX + 1},{bY}" not in newList:
             newList.append(f"{bX},{bY}:{bX + 1},{bY}")
-        if f"{bX + 1},{bY}:{bX + 1},{bY + 1}" not in allBlocks:
+        if f"{bX + 1},{bY}:{bX + 1},{bY + 1}" not in newList:
             newList.append(f"{bX + 1},{bY}:{bX + 1},{bY + 1}")
-        if f"{bX},{bY}:{bX},{bY + 1}" not in allBlocks:
+        if f"{bX},{bY}:{bX},{bY + 1}" not in newList:
             newList.append(f"{bX},{bY}:{bX},{bY + 1}")
-        if f"{bX},{bY + 1}:{bX + 1},{bY + 1}" not in allBlocks:
+        if f"{bX},{bY + 1}:{bX + 1},{bY + 1}" not in newList:
             newList.append(f"{bX},{bY + 1}:{bX + 1},{bY + 1}")
     return newList
 
@@ -36,11 +36,12 @@ while running:
             running=False
         if event.type == pygame.MOUSEBUTTONUP:
             x, y = pygame.mouse.get_pos()
-            allGUIBlocks.append([round_to_multiple(x, 30), round_to_multiple(y, 30)])
-            blockX = int(round(x/30)*30 / 30 - 8 + 100)
-            blockY = int((round(y/30)*30 / 30 - 10) * -1 + 100)
-            allBlocks.append([blockX, blockY])
-            print(blockX, blockY)
+            allGUIBlocks.append([round_to_multiple(x-15, 30), round_to_multiple(y-15, 30)])
+            blockX = int(round((x-15)/30) - 8 + 100)
+            blockY = int((round((y-15)/30) - 10) * -1 + 100)
+            if [blockX, blockY] not in allBlocks:
+                allBlocks.append([blockX, blockY])
+                print(blockX, blockY)
             
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_z:
